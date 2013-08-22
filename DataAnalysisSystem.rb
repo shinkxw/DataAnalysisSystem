cd86b8b094abcef4d5243021f7d69ba28a037364
shinkxw <shink_7770@qq.com>
2013-08-22 17:08:49 +0800

    update some migrate config

--- DataAnalysisSystem.rb
+++ DataAnalysisSystem.rb
@@ -23,54 +23,41 @@ time_proc = Proc.new do |time|
   "CAST('#{time}' AS DateTime)"
   ""
 end
-time_to_str_proc = Proc.new do |time|
-  time
-end
+time_to_6_proc = Proc.new{|time| time.to_s.split(" ")[0].delete("-")[0,5]}
+time_to_8_proc = Proc.new{|time| time.to_s.split(" ")[0].delete("-")}
 
-md.get_table_info(['edu_ele_teacher'])
+md.get_table_info(['edu_ele_student'])
 config = { ID: { fn: '', p: Proc.new{|i| i}},
            SCHOOLID: { fn: '', p: Proc.new{|i| '1'}},
-           GH: { fn: 'ID', p: Proc.new{|s| s}},
+           XH: { fn: 'ID', p: Proc.new{|s| s}},
            XM: { fn: 'Name', p: Proc.new{|s| s}},
            YWXM: { fn: '', p: Proc.new{|i| ''}},
            XMPY: { fn: '', p: Proc.new{|i| ''}},
            CYM: { fn: '', p: Proc.new{|i| ''}},
            SFZJLXM: { fn: '', p: Proc.new{|i| '1'}},
            SFZJH: { fn: 'IDcard', p: Proc.new{|s| s}},
-           CSRQ: { fn: 'Birthday', p: time_to_str_proc},
            XBM: { fn: 'XbID', p: Proc.new{|s| s == '0' ? '1' : '0'}},
-           MZM: { fn: '', p: Proc.new{|i| ''}},
            XXM: { fn: '', p: Proc.new{|i| ''}},
-           JKZKM: { fn: '', p: Proc.new{|i| ''}},
-           HYZTM: { fn: '', p: Proc.new{|i| ''}},
-           ZZMMM: { fn: '', p: Proc.new{|i| ''}},
-           GATQWM: { fn: '', p: Proc.new{|i| ''}},
+           CSRQ: { fn: 'Birthday', p: time_to_8_proc},
+           CSDM: { fn: '', p: Proc.new{|i| ''}},
            JG: { fn: '', p: Proc.new{|i| ''}},
-           GJDQM: { fn: '', p: Proc.new{|i| ''}},
-           CSDXZQHM: { fn: '', p: Proc.new{|i| ''}},
+           MZM: { fn: '', p: Proc.new{|i| ''}},
+           HYZKM: { fn: '', p: Proc.new{|i| ''}},
            XYZJM: { fn: '', p: Proc.new{|i| ''}},
-           JZGHKSZDXZQHM: { fn: '', p: Proc.new{|i| ''}},
+           GATQWM: { fn: '', p: Proc.new{|i| ''}},
+           JKZKM: { fn: '', p: Proc.new{|i| ''}},
+           ZZMMM: { fn: '', p: Proc.new{|i| ''}},
+           HKSZDXZQHM: { fn: '', p: Proc.new{|i| ''}},
            HKLBM: { fn: '', p: Proc.new{|i| ''}},
-           DQZZ: { fn: 'NowAdd', p: Proc.new{|s| s}},
-           DQZZYZBM: { fn: '', p: Proc.new{|i| ''}},
-           CJGZNY: { fn: '', p: Proc.new{|i| ''}},
-           CJNY: { fn: '', p: Proc.new{|i| ''}},
-           LXNY: { fn: 'JoinTime', p: time_to_str_proc},
-           BZLBM: { fn: '', p: Proc.new{|i| ''}},
-           JZGLBM: { fn: '', p: Proc.new{|i| ''}},
-           GWLBM: { fn: '', p: Proc.new{|i| ''}},
-           SFJZJS: { fn: '', p: Proc.new{|i| ''}},
-           SFSSXJS: { fn: '', p: Proc.new{|i| ''}},
-           ZP: { fn: 'Imageurl', p: Proc.new{|s| s}},
-           DQZTM: { fn: '', p: Proc.new{|i| ''}},
-           YDDH: { fn: 'Mobile', p: Proc.new{|s| s}},
-           GDDH: { fn: 'OfficeTel', p: Proc.new{|s| s}},
-           TXDZYZBM: { fn: '', p: Proc.new{|i| ''}},
-           TXDZ: { fn: '', p: Proc.new{|i| ''}},
-           DZXX: { fn: 'Email', p: Proc.new{|s| s}},
+           SFSLDRK: { fn: '', p: Proc.new{|i| ''}},
+           GJDQM: { fn: '', p: Proc.new{|i| ''}},
+           TC: { fn: '', p: Proc.new{|i| ''}},
+           XSLXDH: { fn: 'Mobile', p: Proc.new{|s| s}},
            WLDZ: { fn: '', p: Proc.new{|i| ''}},
            JSTXH: { fn: '', p: Proc.new{|i| ''}},
+           DZXX: { fn: '', p: Proc.new{|i| ''}},
+           ZP: { fn: 'Imageurl', p: Proc.new{|s| s}},
            }
-md.convert_data('EDU_ZZJG_01_01_JZGJBSJ',config)
+md.convert_data('EDU_ZZXS_01_01_XSXX',config)
 
 conn.close
