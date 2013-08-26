17c716cf07caa6e167798cdb1a27bf85e784f5e6
shinkxw <shink_7770@qq.com>
2013-08-26 13:39:53 +0800

    testdatabuilder 0%

--- DataAnalysisSystem.rb
+++ DataAnalysisSystem.rb
@@ -11,35 +11,30 @@ require 'ScriptLoader'#½Å±¾¼ÓÔØÆ÷
 #~ work_area.save_and_close_work_area
 #~ p Time.now - t
 
-#~ conn = SqlServer.new('(local)')
-#~ md = MigrateData.new('CloudEdu',conn)
+conn = SqlServer.new('(local)')
+md = MigrateData.new('CloudEdu',conn)
 
-#~ md.get_table_info(['edu_ele_student'])
-#~ config = { ID: { fn: 'ID', p: Proc.new{|s| s}},
-           #~ SCHOOLID: { fn: '', p: Proc.new{|i| '1'}},
-           #~ ZYXXID: { fn: '', p: Proc.new{|i| ''}},
-           #~ XNID: { fn: '', p: Proc.new{|i| ''}},
-           #~ ZZXQID: { fn: '', p: Proc.new{|i| ''}},
-           #~ JTDZQH: { fn: '', p: Proc.new{|i| ''}},
-           #~ JTXXDZ: { fn: 'HomeAdd', p: Proc.new{|s| s}},
-           #~ HJXXDZ: { fn: '', p: Proc.new{|i| ''}},
-           #~ HJXZ: { fn: '', p: Proc.new{|i| ''}},
-           #~ BYJX: { fn: '', p: Proc.new{|i| ''}},
-           #~ CSBZR: { fn: '', p: Proc.new{|i| ''}},
-           #~ DRZW: { fn: '', p: Proc.new{|i| ''}},
-           #~ YYDR: { fn: '', p: Proc.new{|i| ''}},
-           #~ BZ: { fn: '', p: Proc.new{|i| ''}},
-           #~ BYLX: { fn: '', p: Proc.new{|i| ''}},
-           #~ XSLX: { fn: '', p: Proc.new{|i| ''}},
-           #~ SFZS: { fn: '', p: Proc.new{|i| ''}},
-           #~ XSBMSJ: { fn: '', p: Proc.new{|i| ''}},
-           #~ }
-#~ md.insert_data('EDU_ZZXS_01_A01_XSXX',config)
+md.get_table_info(['edu_ele_student'])
+config = { ID: { fn: 'ID', p: Proc.new{|s| s}},
+           SCHOOLID: { fn: '', p: Proc.new{|i| '1'}},
+           ZYXXID: { fn: '', p: Proc.new{|i| ''}},
+           XNID: { fn: '', p: Proc.new{|i| ''}},
+           ZZXQID: { fn: '', p: Proc.new{|i| ''}},
+           JTDZQH: { fn: '', p: Proc.new{|i| ''}},
+           JTXXDZ: { fn: 'HomeAdd', p: Proc.new{|s| s}},
+           HJXXDZ: { fn: '', p: Proc.new{|i| ''}},
+           HJXZ: { fn: '', p: Proc.new{|i| ''}},
+           BYJX: { fn: '', p: Proc.new{|i| ''}},
+           CSBZR: { fn: '', p: Proc.new{|i| ''}},
+           DRZW: { fn: '', p: Proc.new{|i| ''}},
+           YYDR: { fn: '', p: Proc.new{|i| ''}},
+           BZ: { fn: '', p: Proc.new{|i| ''}},
+           BYLX: { fn: '', p: Proc.new{|i| ''}},
+           XSLX: { fn: '', p: Proc.new{|i| ''}},
+           SFZS: { fn: '', p: Proc.new{|i| ''}},
+           XSBMSJ: { fn: '', p: Proc.new{|i| ''}},
+           }
+md.insert_data('EDU_ZZXS_01_A01_XSXX',config)
 
-#~ conn.close
+conn.close
 
-hash = { "1234" => "1234", "2345" => "2345" }
-p hash.keys
-hash.keys[0] == "123"
-p hash.keys
-p hash
