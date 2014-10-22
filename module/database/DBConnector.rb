2f9dcb32b63cfb4833f15e37db97530ca4001b49
shinkxw <shink_7770@qq.com>
2014-10-22 16:57:19 +0800

    数据库连接器添加对mysql数据库的支持
    需安装mysql的odbc驱动

--- module/database/DBConnector.rb
+++ module/database/DBConnector.rb
@@ -4,6 +4,7 @@ require 'win32ole'
 #选择并返回与数据库实例的连接
 class DBConnector
   attr_reader :host, :db_type, :username, :password
+  @@support_db_type_arr = %w(ss ac mysql)
   #初始化
   def initialize(host, options = {})
     @host = case host
@@ -12,6 +13,7 @@ class DBConnector
             else host
             end
     @db_type = options[:db_type] ? options[:db_type] : 'ss'
+    puts "不支持连接类型为#{@db_type}的数据库！" unless @@support_db_type_arr.include?(@db_type)
     @username = options[:username] ? options[:username] : 'sa'
     @password = options[:password] ? options[:password] : '123456'
   end
@@ -26,6 +28,7 @@ class DBConnector
     case @db_type
     when 'ss' then get_ss_connect_string(db_name)
     when 'ac' then get_ac_connect_string()
+    when 'mysql' then get_mysql_connect_string(db_name)
     end
   end
   #获得sql server连接字符串
@@ -39,6 +42,14 @@ class DBConnector
   end
   #获得access连接字符串
   def get_ac_connect_string(); "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=#{@host};" end
+  def get_mysql_connect_string(db_name)
+    connect_string = "Provider=MSDASQL.1;"
+    connect_string << "Persist Security Info=True;" if is_remote?
+    connect_string << 'Extended Properties="Driver=MySQL ODBC 5.3 ANSI Driver;'
+    connect_string << "SERVER=#{@host};UID=#{@username};PWD=#{@password};DATABASE=#{db_name};"
+    connect_string << 'PORT=3306"'#COLUMN_SIZE_S32=1
+    connect_string
+  end
   #根据host判断是否本地
   def is_local?;@host =~ /^\(local\)/ end
   #根据host判断是否远程
