99433e3d511b45e8343e2cfb6be8c1a44980f9e6
shinkxw <shink_7770@qq.com>
2014-12-17 15:52:27 +0800

    增加迁移配置

--- 数据库迁移.rb
+++ 数据库迁移.rb
@@ -3,79 +3,15 @@
 $root = Dir.pwd
 $:.unshift($root)
 require 'ScriptLoader'#脚本加载器
-load "数据迁移配置\\嘉兴三中门户迁移.rb"
+load "数据迁移配置\\湖州附小门户迁移.rb"
 
-connector = DBConnector.new('E:\经手项目\迁移\嘉兴三中迁移\data.mdb', db_type: 'ac')#, db_type: 'ac'
-DBEntity.open('HanruEdu', connector) do |db|
-  lm_hash_arr = db.get_table_data('PE_Class')#id, fid, name
-  text = DataMigrate.show_data_tree(lm_hash_arr, 183, 'ClassID', 'ParentID', 'ClassName')
-  FileWriter.new("#{$root}/QY/helper.txt").write_str(text)
+connector = DBConnector.new('b')#, db_type: 'ac'
+DBEntity.open('HSFX', connector) do |db|
   
-  config1 = { ID: proc{|d| d['ArticleID']},#
-             SCHOOLID: proc{|d| '27'},#
-             WEBID: proc{|d| '12365'},#
-             TYPENAME: proc{|d| d['Keyword'][0, 20]},#
-             LMID: proc{|d| Qypz.get_lmid(d['ClassID'])},
-             TITLE: proc{|d| d['Title']},#
-             PUBLISHERNAME: proc{|d| 'admin'},#
-             AUTHOR: proc{|d| d['Author']},#
-             AUTHORDEPART: proc{|d| d['CopyFrom']},#
-             PUBLISHDATE: proc{|d, c| c.datetime(d['CreateTime'])},#
-             CONTENT: proc{|d| Qypz.ew_change(d['Content'])},#
-             URL: proc{|d| ''},#
-             IMAGEURL: proc{|d| Qypz.img(d['DefaultPicUrl'])},#
-             ATTACHMENTNAME: proc{|d| ''},#
-             ATTACHMENT: proc{|d| ''},#UploadFiles
-             CHICKNUB: proc{|d| d['Hits'].to_i},#
-             AUDITSTATU: proc{|d| '1'},#
-             AUDITOR: proc{|d| 'admin'},#
-             AUDITORNAME: proc{|d| 'admin'},#
-             AUDITTIME: proc{|d, c| c.datetime(d['CreateTime'])},#
-             REMARK: proc{|d| d['TitleIntact']},#
-             SUMMARY: proc{|d| d['Subheading']},#
-             DISPLAYTYPE: proc{|d| '0'},#
-             OPENFLAG: proc{|d| '1'},#
-             LLQX: proc{|d| '0'},#
-             INDEXSHOW: proc{|d| '0'},#
-             SFZD: proc{|d| '1'},#
-             ZDYXJ: proc{|d| '0'},#
-           }
-  data_hash_arr = db.get_table_data('PE_Article')
-  out_hash_arr = DataMigrate.convert_data(data_hash_arr, config1)
+  data_hash_arr = db.get_table_data('TD_Article')
+  out_hash_arr = DataMigrate.convert_data(data_hash_arr, $wz_config)
   out_hash_arr.delete_if {|hash| hash[:LMID] == '-1'}
   DataMigrate.export_text('insert_data', out_hash_arr, 'EDU_WZXT_MHXT_WZWZ')
   
-  config2 = { ID: proc{|d| 4000 + d['ID']},#
-             SCHOOLID: proc{|d| '27'},#
-             WEBID: proc{|d| '12365'},#
-             TYPENAME: proc{|d| ''},#
-             LMID: proc{|d| '1209'},
-             TITLE: proc{|d| d['Title']},#
-             PUBLISHERNAME: proc{|d| 'admin'},#
-             AUTHOR: proc{|d| d['Author']},#
-             AUTHORDEPART: proc{|d| ''},#
-             PUBLISHDATE: proc{|d, c| c.datetime(d['DateAndTime'])},#
-             CONTENT: proc{|d| Qypz.ew_change(d['Content'])},#
-             URL: proc{|d| ''},#
-             IMAGEURL: proc{|d| ''},#
-             ATTACHMENTNAME: proc{|d| ''},#
-             ATTACHMENT: proc{|d| ''},#
-             CHICKNUB: proc{|d| '1'},#
-             AUDITSTATU: proc{|d| '1'},#
-             AUDITOR: proc{|d| 'admin'},#
-             AUDITORNAME: proc{|d| 'admin'},#
-             AUDITTIME: proc{|d, c| c.datetime(d['DateAndTime'])},#
-             REMARK: proc{|d| ''},#
-             SUMMARY: proc{|d| ''},#
-             DISPLAYTYPE: proc{|d| '0'},#
-             OPENFLAG: proc{|d| '1'},#
-             LLQX: proc{|d| '0'},#
-             INDEXSHOW: proc{|d| '0'},#
-             SFZD: proc{|d| '1'},#
-             ZDYXJ: proc{|d| '0'},#
-           }
-  #~ data_hash_arr = db.get_table_data('PE_Announce')
-  #~ out_hash_arr = DataMigrate.convert_data(data_hash_arr, config2)
-  #~ DataMigrate.export_text('insert_data', out_hash_arr, 'EDU_WZXT_MHXT_WZWZ')
 end
 #delete [EDU_WZXT_MHXT_WZWZ] where [SCHOOLID] = 27 and [WEBID] = 12365
---
+++ 数据迁移配置/湖州附小门户迁移.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/env ruby -w
+# encoding: GBK
+
+class Qypz
+  ary = ['000064','1288','000014','1238','000056','1279','000022','1233','000063','1287','000036','1256','000032','1252','000002','1120','000058','1282','000008','1126','000055','1278',
+         '000035','1255','000028','1247','000026','1245','000054','1277','000049','1272','000010','1133','000061','1285','000009','1232','000042','1264','000005','1123','000040','1262',
+         '000044','1267','000033','1253','000067','1241','000059','1283','000030','1250','000024','1257','000004','1122','000045','1268','000043','1266','000006','1124','000025','1258',
+         '000015','1239','000052','1275','000051','1274','000041','1263','000068','1265','000031','1251','000062','1286','000053','1276','000046','1269','000012','1236','000048','1271',
+         '000037','1259','000021','1248','000003','1121','000034','1254','000029','1249','000020','1235','000013','1237','000057','1280','000011','1118','000050','1273','000047','1270',
+         '000007','1125','000001','1119','000017','1242','000018','1243','000027','1246','000023','1234','000039','1261','000038','1260','000019','1244','000060','1284','000016','1240']
+  @@hash = Hash[*ary]
+  @@hash.default = '-1'
+    
+  def self.get_lmid(classid); @@hash[classid] end
+  
+  def self.img(str)
+    if (str != "" && str != nil)
+      str = str.gsub(/\/JxszWeb\/UploadFiles\//, '')
+      str = ('/WZXT/Content/eWebeditor/uploadfile/' + str)
+    else
+      str = nil
+    end
+    str
+  end
+  
+  def self.ew_change(str)
+    str = str.gsub(/'/, "''")
+    str = str.gsub(/\[InstallDir_ChannelDir\]\{\$UploadDir\}/, "/WZXT/Content/eWebeditor/uploadfile")
+    str = str.gsub(/http:\/\/10.10.11.3:8888\/JxszWeb\/\{\$UploadDir\}/, "/WZXT/Content/eWebeditor/uploadfile")
+    str = str.gsub(/\/JxszWeb\/UploadFiles/, "/WZXT/Content/eWebeditor/uploadfile")
+    str = str.gsub(/\[InstallDir_ChannelDir\]ShowArticle\.asp\?ArticleID=/, "/WZXT/Sites/jxszmh/NewsDetails?did=")
+    str = str.gsub(/http:\/\/www.jxsz.com\/JxszWeb\/ShowArticle\.asp\?ArticleID=/, "/WZXT/Sites/jxszmh/NewsDetails?did=")
+    str = str.gsub(/\/JxszWeb\/ShowArticle\.asp\?ArticleID=/, "/WZXT/Sites/jxszmh/NewsDetails?did=")
+    str
+  end
+end
+
+$wz_config = { ID: proc{|d| d['ArticleID'].to_i},
+          SCHOOLID: proc{|d| '1'},#101
+          WEBID: proc{|d| '12363'},
+          TYPENAME: proc{|d| ''},
+          LMID: proc{|d| Qypz.get_lmid(d['TypeID'])},
+          TITLE: proc{|d| d['Title']},
+          PUBLISHERNAME: proc{|d| ''},
+          AUTHOR: proc{|d| d['Author']},
+          AUTHORDEPART: proc{|d| ''},
+          PUBLISHDATE: proc{|d, c| c.str_to_datetime(d['InputTime'])},
+          CONTENT: proc{|d| Qypz.ew_change(d['Detail'])},
+          URL: proc{|d| ''},
+          IMAGEURL: proc{|d| d['ImgID']},#
+          ATTACHMENTNAME: proc{|d| ''},
+          ATTACHMENT: proc{|d| ''},
+          CHICKNUB: proc{|d| d['ReadTimes']},
+          AUDITSTATU: proc{|d| '1'},
+          AUDITOR: proc{|d| 'admin'},
+          AUDITORNAME: proc{|d| 'admin'},
+          AUDITTIME: proc{|d, c| c.str_to_datetime(d['UpdateTime'] ? d['UpdateTime'] : d['InputTime'])},
+          REMARK: proc{|d| ''},
+          SUMMARY: proc{|d| ''},
+          DISPLAYTYPE: proc{|d| '0'},
+          OPENFLAG: proc{|d| '1'},
+          LLQX: proc{|d| '0'},
+          INDEXSHOW: proc{|d| '0'},
+          SFZD: proc{|d| '1'},
+          ZDYXJ: proc{|d| '0'},
+        }
+$yh_config = { LOGINNAME: proc{|d| d['username']},#
+          SCHOOLID: proc{|d| '28'},#
+          APPID: proc{|d| '10'},#
+          PWD: proc{|d| d['password']},#
+          STATUS: proc{|d| '1'},#
+          USERTYPE: proc{|d| '0'},#
+          USERID: proc{|d| '0'},#
+          ROLEIDLst: proc{|d| '0'},#
+          XM: proc{|d| ''},#
+          XB: proc{|d| d['gender']},#
+          QQ: proc{|d| ''},#
+          DZYJ: proc{|d| d['email']},#
+          LXDH: proc{|d| ''},#
+          ZJDLSJ: proc{|d, c| c.int_to_datetime(d['lastvisit'])},#
+          DLCGCS: proc{|d| d['loginnum']},#
+          YHCJSJ: proc{|d, c| c.int_to_datetime(d['regdate'])},#
+          YHRY: proc{|d| '0'},#
+          YHJF: proc{|d| '0'},#
+        }
