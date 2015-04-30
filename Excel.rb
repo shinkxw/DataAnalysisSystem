ef2cd81419fd79f0e0906b7fcc247653e2064dcd
shinkxw <shink_7770@qq.com>
2015-04-30 16:07:05 +0800

    表结构更新

--- Excel.rb
+++ Excel.rb
@@ -4,13 +4,14 @@ $root = Dir.pwd
 $:.unshift($root)
 require 'ScriptLoader'#脚本加载器
 
-ExcelLoader.open_sheet("#{Dir.pwd}/班级名称对应表.xlsx", 'Sheet1') do |sd_arr|
+path = 'C:/Users/Administrator/Desktop'
+ExcelLoader.open_sheet("#{path}/班级名称对应表.xlsx", 'Sheet1') do |sd_arr|
   out_str, last_class_name = '', ''
   sd_arr.each do |row|
     if (row[0] != last_class_name)
-      out_str << "#{row[0]}      #{row[1]}      #{row[2]}\n"
+      out_str << "UPDATE [HANRUEDU].[dbo].[EDU_ZZJX_02_02_ZZBJ] SET [XZBMC] = '#{row[0]}' WHERE [XZBMC] = '#{row[1]}'\n"
       last_class_name = row[0]
     end
   end
-  File.open("#{Dir.pwd}/bjzh.txt","w"){|file| file.puts(out_str)}
+  File.open("#{path}/bjzh.txt","w"){|file| file.puts(out_str)}
 end
