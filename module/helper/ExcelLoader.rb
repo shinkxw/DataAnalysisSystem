4ada3f1c2f72027d0bea14be01da12a57a6c27d2
shink <shink_7770@qq.com>
2015-09-10 16:14:01 +0800

    在关闭excel后删除进程

--- module/helper/ExcelLoader.rb
+++ module/helper/ExcelLoader.rb
@@ -1,13 +1,20 @@
 #!/usr/bin/env ruby -w
 # encoding: GBK
 require 'win32ole'
+
 class ExcelLoader
   def self.open(path)
+    mgmt = WIN32OLE.connect('winmgmts:\\\\.')
+    q_str = "Select * from Win32_Process Where Name ='EXCEL.EXE'"
+    ph_arr = mgmt.ExecQuery(q_str).each.map{|p| p.handle}
+    
     excel = WIN32OLE::new('excel.Application').tap{|e| e.Visible = false}
     excel.WorkBooks.Open(path)
     yield excel
     excel.WorkBooks.Close
     excel.Quit
+    #É¾excel½ø³Ì
+    mgmt.ExecQuery(q_str).each{|p| p.Terminate() unless ph_arr.include?(p.handle)}
   end
   def self.open_sheet(path, sheet_name);open(path){|excel| yield load_sheet(excel, sheet_name)} end
   def self.get_sheet_data(path, sheet_name);open(path){|excel| return load_sheet(excel, sheet_name)} end
---
+++ 文档/其他脚本/kill_excel.rb
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+require "win32ole"  
+
+mgmt = WIN32OLE.connect('winmgmts:\\\\.')
+#~ str = "Select * from Win32_Process Where Name ='EXCEL.EXE'"
+#~ mgmt.ExecQuery(str).each{|item| item.Terminate()}
+
+mgmt.ExecQuery("Select * from Win32_Process").each do |item|
+  p "#{item.name}   #{item.Handle}"
+end
